ddba7ec9aa4e9261d9ba6af1143059a5aa2ed195
ahmedhamdy <ahmedhamdy7796@gmail.com>
2024-03-07 17:13:14 +0200

    add model for user accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.IntegerField(max_length=2)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    addres = models.CharField(max_length=50, null=True, blank=True)
+    experience = models.CharField(max_length=200, null= True, blank=True) 
